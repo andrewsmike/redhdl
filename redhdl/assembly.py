17948d8a7574151c81a1a1e38910d5288a71338f
Mike Andrews <andrewsm13@gmail.com>
2023-05-11 19:46:51 -0400

    Remove naive pathfinding, massively tweak heuristics.

--- redhdl/assembly.py
+++ redhdl/assembly.py
@@ -1,18 +1,22 @@
 from dataclasses import dataclass
+from math import log2
 from random import seed
 
 from redhdl.bussing import BussingError
 from redhdl.local_search import LocalSearchProblem, sim_annealing_searched_solution
 from redhdl.naive_bussing import (
-    PartialBusPaths,
+    PartialPinBuses,
     bussed_placement_schematic,
     bussing_avg_length,
     bussing_max_length,
     bussing_min_avg_length,
     bussing_min_max_length,
-    collision_count,
-    dest_pin_relaxed_bus_path,
-    interrupted_pin_line_of_sight_count,
+    crossed_bus_pct,
+    dest_pin_buses,
+    misaligned_bus_pct,
+    pin_pair_excessive_downwards_pct,
+    pin_pair_interrupted_line_of_sight_pct,
+    stride_aligned_bus_pct,
 )
 from redhdl.netlist import InstanceId, Netlist
 from redhdl.netlist_template import (
@@ -24,8 +28,8 @@ from redhdl.netlist_template import (
 )
 from redhdl.placement import (
     InstancePlacement,
+    avg_instance_buffer_blocks,
     display_placement,
-    instance_buffer_blocks,
     mutated_placement,
     netlist_random_placement,
     placement_compactness_score,
@@ -36,22 +40,29 @@ from redhdl.schematic import Schematic, save_schem
 
 def unbussable_placement_cost(netlist: Netlist, placement: InstancePlacement) -> float:
     return (
-        bussing_min_avg_length(netlist, placement) / 2
-        + bussing_min_max_length(netlist, placement) / 2
-        - placement_compactness_score(netlist, placement)
-        + interrupted_pin_line_of_sight_count(netlist, placement) / 2
-        + (20 * 4 - instance_buffer_blocks(netlist, placement) * 20)
+        log2(bussing_min_avg_length(netlist, placement)) * 5
+        + log2(bussing_min_max_length(netlist, placement)) * 5
+        + 20 / (placement_compactness_score(netlist, placement) + 10)
+        + pin_pair_interrupted_line_of_sight_pct(netlist, placement) * 10
+        + (6 - avg_instance_buffer_blocks(netlist, placement)) * 100
+        + misaligned_bus_pct(netlist, placement) * 10
+        + (1 - stride_aligned_bus_pct(netlist, placement)) * 10
+        + crossed_bus_pct(netlist, placement) * 40
+        + pin_pair_excessive_downwards_pct(netlist, placement) * 40
     )
 
 
 def bussable_placement_cost(
-    netlist: Netlist, placement: InstancePlacement, bussing: PartialBusPaths
+    netlist: Netlist, placement: InstancePlacement, bussing: PartialPinBuses
 ) -> float:
     return (
-        collision_count(bussing) * 20
-        - placement_compactness_score(netlist, placement)
+        # collision_count(bussing) * 20
+        -placement_compactness_score(netlist, placement)
         + bussing_avg_length(bussing) / 6
         + bussing_max_length(bussing) / 6
+        + (6 - avg_instance_buffer_blocks(netlist, placement)) * 5
+        + misaligned_bus_pct(netlist, placement) * 10
+        + pin_pair_excessive_downwards_pct(netlist, placement) * 40
     )
 
 
@@ -59,35 +70,48 @@ def bussable_placement_cost(
 class HeuristicBussingPlacementProblem(LocalSearchProblem[InstancePlacement]):
     netlist: Netlist
     initial_placement: InstancePlacement
+    mutations_per_step: int = 2
 
     def random_solution(self) -> InstancePlacement:
         return self.initial_placement
 
     def mutated_solution(self, solution: InstancePlacement) -> InstancePlacement:
-        return mutated_placement(solution)
+        for i in range(self.mutations_per_step):
+            solution = mutated_placement(solution)
+
+        return solution
 
     def solution_cost(self, solution: InstancePlacement) -> float:
         return unbussable_placement_cost(self.netlist, solution)
 
 
+PCT_RANDOM_PLACEMENTS = 0.1
+
+
 def mutated_unbussable_placement(
-    netlist: Netlist, placement: InstancePlacement
+    netlist: Netlist, placement: InstancePlacement, total_rounds: int
 ) -> InstancePlacement:
     """Unbussable placement mutation: Optimize placement / pin distance / line of sight a few steps."""
     problem = HeuristicBussingPlacementProblem(netlist, initial_placement=placement)
-    solution_placement = sim_annealing_searched_solution(
-        problem, total_rounds=250, restarts=2
+    next_placement = sim_annealing_searched_solution(
+        problem,
+        total_rounds=total_rounds,
+        restarts=2,
     )
-    if solution_placement != placement:
-        return solution_placement
+    if next_placement == placement:  # Sim annealing didn't like the results.
+        print("No improvement.")
+        for i in range(3):
+            placement = mutated_placement(placement)
+
+        return placement
     else:
-        return mutated_placement(mutated_placement(placement))
+        return next_placement
 
 
 def mutated_bussable_placement(
     netlist: Netlist,
     placement: InstancePlacement,
-    bussing: PartialBusPaths,
+    bussing: PartialPinBuses,
 ) -> InstancePlacement:
     return mutated_placement(mutated_placement(placement))
 
@@ -97,14 +121,22 @@ class BussingPlacementProblem(LocalSearchProblem[InstancePlacement]):
     netlist: Netlist
 
     def random_solution(self) -> InstancePlacement:
-        return netlist_random_placement(self.netlist)
+        return mutated_unbussable_placement(
+            self.netlist,
+            netlist_random_placement(self.netlist),
+            total_rounds=8192,
+        )
 
     def mutated_solution(self, solution: InstancePlacement) -> InstancePlacement:
         try:
             # This call should be cached and return (or raise) immediately.
-            bussing = dest_pin_relaxed_bus_path(self.netlist, solution)
+            bussing = dest_pin_buses(self.netlist, solution)
         except BussingError:
-            return mutated_unbussable_placement(self.netlist, solution)
+            return mutated_unbussable_placement(
+                self.netlist,
+                solution,
+                total_rounds=512,
+            )
         else:
             return mutated_bussable_placement(self.netlist, solution, bussing)
 
@@ -115,12 +147,12 @@ class BussingPlacementProblem(LocalSearchProblem[InstancePlacement]):
             return 1_000_000
 
         try:
-            bussing = dest_pin_relaxed_bus_path(self.netlist, solution)
-        except BussingError:
-            print("Unbussable!")
+            bussing = dest_pin_buses(self.netlist, solution)
+        except BussingError as e:
+            print(f"Unbussable! Error: {e}")
             return 100_000 + unbussable_placement_cost(self.netlist, solution)
         else:
-            print(f"Bussable! Collision count: {collision_count(bussing)}")
+            print("Bussable!")  # Collision count: {collision_count(bussing)}")
             return bussable_placement_cost(self.netlist, solution, bussing)
 
 
@@ -131,14 +163,16 @@ def assembled_circuit_schem(
     netlist = netlist_from_simple_spec(
         instance_config=instance_config,
         network_specs=network_specs,
+        output_port_bitwidths={"out": 8},
     )
 
     def solution_schematic(placement: InstancePlacement) -> Schematic:
-        bus_paths = dest_pin_relaxed_bus_path(netlist, placement)
-        print(f"Collision count: {collision_count(bus_paths)}")
-        return bussed_placement_schematic(netlist, placement, bus_paths)
+        pin_buses = dest_pin_buses(netlist, placement)
+        # print(f"Collision count: {collision_count(pin_buses)}")
+        return bussed_placement_schematic(netlist, placement, pin_buses)
 
-    TOTAL_ROUNDS = 1_000
+    # TOTAL_ROUNDS = 1_000
+    TOTAL_ROUNDS = 15
 
     def checkpoint(round: int, placement: InstancePlacement, cost: float):
         path = f"checkpoints/output_{round}.schem"
@@ -146,10 +180,16 @@ def assembled_circuit_schem(
             f"[{round + 1}/{TOTAL_ROUNDS}] Saving solution with cost {cost} to {path}..."
         )
         try:
+            from pprint import pprint
+
+            print("Placement:")
+            pprint(placement)
             schem = solution_schematic(placement)
             save_schem(schem, path)
-        except BaseException as e:
-            print(f"Failed to save: {e}")
+            # from pprint import pprint
+            # pprint(schem)
+        except BussingError as e:
+            print(f"Failed to save due to bussing error: {e}")
 
     seed(0xDEADBEEF)
     placement_problem = BussingPlacementProblem(netlist)
@@ -158,7 +198,7 @@ def assembled_circuit_schem(
         total_rounds=TOTAL_ROUNDS,
         show_progressbar=True,
         rounds_per_print=1,
-        rounds_per_checkpoint=50,
+        rounds_per_checkpoint=1,
         checkpoint_func=checkpoint,
     )
     print(f"Best cost: {placement_problem.solution_cost(placement)}")
--- redhdl/naive_bussing.py
+++ redhdl/naive_bussing.py
@@ -1,178 +1,83 @@
-from collections import Counter
-from dataclasses import dataclass
-from functools import reduce, wraps
+from functools import reduce
 import operator
-from random import choice
 
-from frozendict import frozendict
-
-from redhdl.bussing import BussingError, BussingImpossibleError, BussingTimeoutError
-from redhdl.local_search import LocalSearchProblem, sim_annealing_searched_solution
+from redhdl.caching import first_id_cached
 from redhdl.netlist import Netlist, PinId
-from redhdl.path_search import (
-    NoSolutionError,
-    PathSearchProblem,
-    SearchTimeoutError,
-    a_star_bfs_searched_solution,
-)
 from redhdl.placement import (
     InstancePlacement,
-    PinPosPair,
+    placement_pin_seq_points,
     placement_region,
     placement_schematic,
+    source_dest_pin_id_seq_pairs,
     source_dest_pin_pos_pairs,
 )
-from redhdl.positional_data import PositionalData
-from redhdl.region import (
-    PointRegion,
-    Pos,
-    RectangularPrism,
-    Region,
-    direction_unit_pos,
-    xz_directions,
-)
-from redhdl.schematic import Block, Schematic
+from redhdl.redstone_bussing import RedstoneBussing, redstone_bussing
+from redhdl.region import CompositeRegion, Pos, RectangularPrism
+from redhdl.schematic import Schematic
 
-
-@dataclass
-class PathFindingProblem(PathSearchProblem[Pos, Pos]):
-    start_point: Pos
-    stop_point: Pos
-    blocked_points: frozenset[Pos]
-
-    def initial_state(self) -> Pos:
-        return self.start_point
-
-    def state_actions(self, state: Pos) -> list[Pos]:
-        return [
-            next_pos
-            for unit_pos in direction_unit_pos.values()
-            if (
-                ((next_pos := unit_pos + state) == self.stop_point)
-                or (next_pos not in self.blocked_points)
-            )
-        ]
-
-    def state_action_result(self, state: Pos, action: Pos) -> Pos:
-        return action
-
-    def state_action_cost(self, state: Pos, action: Pos) -> float:
-        return 1
-
-    def is_goal_state(self, state: Pos) -> bool:
-        return state == self.stop_point
-
-    def min_cost(self, state: Pos) -> float:
-        return (state - self.stop_point).l1()
-
-
-def bus_path(
-    blocked_regions: Region,
-    source_point: Pos,
-    dest_point: Pos,
-) -> list[Pos]:
-    problem = PathFindingProblem(
-        start_point=source_point,
-        stop_point=dest_point,
-        blocked_points=blocked_regions.region_points(),
-    )
-
-    try:
-        return [source_point] + a_star_bfs_searched_solution(problem, max_steps=4_000)
-    except SearchTimeoutError as e:
-        raise BussingTimeoutError(f"Failed to find A* bus route: {e}")
-    except NoSolutionError:
-        raise BussingImpossibleError(
-            f"No way to bus between {dest_point} and {source_point}."
-        )
-
-
-def first_id_cached(func):
-    func._cache = {}
-
-    @wraps(func)
-    def wrapper(id_obj, *args, **kwargs):
-        key = (id(id_obj), tuple(args), tuple(sorted(kwargs.items())))
-        if key not in func._cache:
-            try:
-                func._cache[key] = (True, func(id_obj, *args, **kwargs))
-            except BaseException as e:
-                func._cache[key] = (False, e)
-
-        success, result = func._cache[key]
-        if success:
-            return result
-        else:
-            raise result
-
-    return wrapper
-
-
-def wire_region(path: list[Pos]) -> Region:
-    return PointRegion(
-        frozenset(
-            point + direction_unit_pos[xz_dir] + y_offset
-            for point in path
-            for xz_dir in xz_directions
-            for y_offset in (Pos(0, -1, 0), Pos(0, 0, 0), Pos(0, 1, 0))
-        )
-    )
-
-
-BusPaths = dict[PinId, list[Pos]]
-PartialBusPaths = dict[PinId, list[Pos] | None]
+PinBuses = dict[PinId, RedstoneBussing]
+PartialPinBuses = dict[PinId, RedstoneBussing | None]
 
 
 @first_id_cached
-def dest_pin_bus_path(
+def dest_pin_buses(
     netlist: Netlist,
     placement: InstancePlacement,
-) -> BusPaths:
-    blocks_region = placement_region(netlist, placement).xz_padded(1)
+) -> PinBuses:
+    instances_region = placement_region(netlist, placement).xz_padded(1)
+    instance_region_points = instances_region.region_points()
 
-    dest_pin_bus_path: dict[PinId, list[Pos]] = {}
+    dest_pin_buses: PinBuses = {}
     for pin_pos_pair in source_dest_pin_pos_pairs(netlist, placement):
-        path = bus_path(
-            blocked_regions=blocks_region,
-            source_point=pin_pos_pair.source_pin_pos,
-            dest_point=pin_pos_pair.dest_pin_pos,
-        )
-        dest_pin_bus_path[pin_pos_pair.dest_pin_id] = path
-        blocks_region = blocks_region | wire_region(path)
+        other_buses = reduce(operator.or_, dest_pin_buses.values(), RedstoneBussing())
+        try:
+            bussing = redstone_bussing(
+                start_pos=pin_pos_pair.source_pin_pos,
+                end_pos=pin_pos_pair.dest_pin_pos,
+                start_xz_dir=pin_pos_pair.source_pin_facing,
+                end_xz_dir=pin_pos_pair.dest_pin_facing,
+                instance_points=instance_region_points,
+                other_buses=other_buses,
+                max_steps=512,
+            )
+        except BaseException:
+            from time import time
 
-    return dest_pin_bus_path
+            from redhdl.schematic import save_schem
 
+            save_schem(
+                bussed_placement_schematic(netlist, placement, dest_pin_buses),
+                f"checkpoints/failed_{time()}.schem",
+            )
+            raise
 
-def bus_trace_pos_blocks(bus_paths: BusPaths) -> PositionalData[Block]:
-    return PositionalData(
-        {
-            bus_point: Block("minecraft:blue_wool", frozendict())
-            for pin_id, bus_points in bus_paths.items()
-            for bus_point in bus_points
-        }
-    )
+        dest_pin_buses[pin_pos_pair.dest_pin_id] = bussing
+
+    return dest_pin_buses
 
 
 def bussed_placement_schematic(
     netlist: Netlist,
     placement: InstancePlacement,
-    bus_paths: BusPaths,
+    pin_buses: PinBuses,
 ) -> Schematic:
     schem = placement_schematic(netlist, placement)
-    schem.pos_blocks |= bus_trace_pos_blocks(bus_paths)
+    for bus in pin_buses.values():
+        schem |= bus.schem()
+
     return schem
 
 
-def bussing_avg_length(bus_paths: PartialBusPaths) -> float:
+def bussing_avg_length(pin_buses: PartialPinBuses) -> float:
     successful_bus_path_lengths = [
-        len(bus_points) for bus_points in bus_paths.values() if bus_points is not None
+        len(bus.element_sig_strengths) for bus in pin_buses.values() if bus is not None
     ]
     return sum(successful_bus_path_lengths) / len(successful_bus_path_lengths)
 
 
-def bussing_max_length(bus_paths: PartialBusPaths) -> float:
+def bussing_max_length(pin_buses: PartialPinBuses) -> float:
     successful_bus_path_lengths = [
-        len(bus_points) for bus_points in bus_paths.values() if bus_points is not None
+        len(bus.element_sig_strengths) for bus in pin_buses.values() if bus is not None
     ]
     return max(successful_bus_path_lengths)
 
@@ -193,7 +98,7 @@ def bussing_min_max_length(netlist: Netlist, placement: InstancePlacement) -> fl
     return max(l1s)
 
 
-def interrupted_pin_line_of_sight_count(
+def pin_pair_interrupted_line_of_sight_pct(
     netlist: Netlist, placement: InstancePlacement
 ) -> float:
     instance_regions = placement_region(netlist, placement)
@@ -203,225 +108,132 @@ def interrupted_pin_line_of_sight_count(
         if RectangularPrism(
             pin_pos_pair.source_pin_pos, pin_pos_pair.dest_pin_pos
         ).intersects(instance_regions)
-    )
+    ) / len(list(source_dest_pin_pos_pairs(netlist, placement)))
 
 
-@dataclass
-class RelaxedPathFindingProblem(PathSearchProblem[Pos, Pos]):
-    start_point: Pos
-    stop_point: Pos
-    instance_points: frozenset[Pos]
-    wire_points: frozenset[Pos]
-    collision_cost: float = 6
-
-    def initial_state(self) -> Pos:
-        return self.start_point
-
-    def state_actions(self, state: Pos) -> list[Pos]:
-        return [
-            next_pos
-            for unit_pos in direction_unit_pos.values()
-            if (
-                (next_pos := unit_pos + state) == self.stop_point
-                or next_pos not in self.instance_points
-            )
-        ]
-
-    def state_action_result(self, state: Pos, action: Pos) -> Pos:
-        return action
-
-    def state_action_cost(self, state: Pos, action: Pos) -> float:
-        return 1 if (action not in self.wire_points) else self.collision_cost
-
-    def is_goal_state(self, state: Pos) -> bool:
-        return state == self.stop_point
-
-    def min_cost(self, state: Pos) -> float:
-        return (state - self.stop_point).l1()
+def pin_pair_excessive_downwards_pct(
+    netlist: Netlist, placement: InstancePlacement
+) -> float:
+    return sum(
+        1
+        for pin_pos_pair in source_dest_pin_pos_pairs(netlist, placement)
+        if abs(delta := pin_pos_pair.dest_pin_pos - pin_pos_pair.source_pin_pos)
+        .xz_pos()
+        .l1()
+        < abs(delta.y)
+        and delta.y < 0
+    ) / len(list(source_dest_pin_pos_pairs(netlist, placement)))
 
 
-def relaxed_bus_path(
-    instance_regions: Region,
-    wire_regions: Region,
-    source_point: Pos,
-    dest_point: Pos,
-    collision_cost: float,
-) -> list[Pos]:
-    problem = RelaxedPathFindingProblem(
-        start_point=source_point,
-        stop_point=dest_point,
-        instance_points=instance_regions.region_points(),
-        wire_points=wire_regions.region_points(),
-        collision_cost=collision_cost,
-    )
+def misaligned_bus_pct(netlist: Netlist, placement: InstancePlacement) -> int:
+    """
+    The percent of port pairs that are misaligned with each other.
 
-    try:
-        return [source_point] + a_star_bfs_searched_solution(problem, max_steps=18_000)
-    except SearchTimeoutError as e:
-        raise BussingTimeoutError(f"Failed to find A* bus route: {e}")
-    except NoSolutionError:
-        raise BussingImpossibleError(
-            f"No way to bus between {dest_point} and {source_point}."
+    Shifting as an expensive and complicated operation. If ports have the same
+    alignment, reward placements that exactly align them.
+
+    Output range [0, 1], with 1 being "all misaligned" and 0 being "none misaligned".
+    """
+    misaligned_bus_count = 0
+    bus_count = 0
+
+    for source_pin_id_seq, dest_pin_id_seq in source_dest_pin_id_seq_pairs(netlist):
+        bus_count += 1  # Denominator.
+
+        source_pin_points = placement_pin_seq_points(
+            netlist, source_pin_id_seq, placement
         )
+        dest_pin_points = placement_pin_seq_points(netlist, dest_pin_id_seq, placement)
 
-
-@first_id_cached
-def collision_count(bus_paths: PartialBusPaths) -> int:
-    taken_points = PointRegion(frozenset())
-
-    collision_count = 0
-    for path in bus_paths.values():
-        if path is None:
+        if source_pin_points.step != dest_pin_points.step:
             continue
-        bus_points = wire_region(path)
-        collision_count += len(bus_points & taken_points)
-        taken_points |= bus_points
 
-    return collision_count
+        delta = dest_pin_points[0] - source_pin_points[0]
 
+        if (delta * source_pin_points.step).l1() == 0:
+            misaligned_bus_count += 1
 
-@first_id_cached
-def too_far_to_bus_count(bus_paths: PartialBusPaths) -> int:
-    return sum(path is None for path in bus_paths.values())
+    return misaligned_bus_count // bus_count
 
 
-MAX_RECENT_FAILURES = 64
-_recent_dest_pin_bus_failures: list[PinId] = []
+def stride_aligned_bus_pct(netlist: Netlist, placement: InstancePlacement) -> float:
+    """
+    The percent of buses that have the same input/output strides.
+
+    This is a pretty decent metric of whether two buses are facing each other nicely.
+
+    Output range [0, 1], where 0 is "no bus pairs are stride-aligned" and 1 is
+    "all bus pairs are stride-aligned".
+    """
+    stride_aligned_bus_count = 0
+    bus_count = 0
+
+    for source_pin_id_seq, dest_pin_id_seq in source_dest_pin_id_seq_pairs(netlist):
+        bus_count += 1  # Denominator.
+
+        source_pin_points = placement_pin_seq_points(
+            netlist, source_pin_id_seq, placement
+        )
+        dest_pin_points = placement_pin_seq_points(netlist, dest_pin_id_seq, placement)
+
+        stride_aligned_bus_count += source_pin_points.step == dest_pin_points.step
+
+    return stride_aligned_bus_count / bus_count
 
 
-def recent_dest_pin_bus_failure_count() -> dict[PinId, int]:
-    return Counter(_recent_dest_pin_bus_failures)
+def crossed_bus_pct(netlist: Netlist, placement: InstancePlacement) -> float:
+    """
+    The percent of buses whose line-of-sight crosess through other buses' lines-of-sight.
 
+    This placement would have a 50% crossed_bus_pct:
 
-def record_dest_pin_bus_failure(pin_id: PinId) -> None:
-    global _recent_dest_pin_bus_failures
-    _recent_dest_pin_bus_failures = _recent_dest_pin_bus_failures[
-        -(MAX_RECENT_FAILURES - 1) :
-    ] + [pin_id]
+    A-` /-C--E
+       X
+    B-/ `-D--F
 
+    This is a heurisic measure today, based on bounding boxes.
+    A real version would be more precise.
 
-def random_relaxed_bus_paths(
-    instance_regions: Region,
-    pin_pos_pairs: set[PinPosPair],
-    collision_cost: float,
-) -> PartialBusPaths:
-    wire_regions: Region = PointRegion(frozenset())
+    Output range is [0, 1], where 0 is "no buses' lines-of-sight collide", and 1 is
+    "all buses' lines-of-sight collide with at least one other bus's line-of-sight".
+    """
+    port_pair_region = {}
 
-    ordered_pin_pos_pairs = sorted(
-        pin_pos_pairs,
-        key=lambda pin_pos_pair: (
-            -recent_dest_pin_bus_failure_count()[pin_pos_pair.dest_pin_id],
-            pin_pos_pair,
-        ),
-    )
+    for source_pin_id_seq, dest_pin_id_seq in source_dest_pin_id_seq_pairs(netlist):
+        source_pin_points = placement_pin_seq_points(
+            netlist, source_pin_id_seq, placement
+        )
+        dest_pin_points = placement_pin_seq_points(netlist, dest_pin_id_seq, placement)
 
-    bus_path: PartialBusPaths = {
-        pin_pos_pair.dest_pin_id: None for pin_pos_pair in ordered_pin_pos_pairs
-    }
-    for pin_pos_pair in ordered_pin_pos_pairs:
-        try:
-            path = relaxed_bus_path(
-                instance_regions=instance_regions,
-                wire_regions=wire_regions,
-                source_point=pin_pos_pair.source_pin_pos,
-                dest_point=pin_pos_pair.dest_pin_pos,
-                collision_cost=collision_cost,
+        relevant_points = {
+            source_pin_points.start,
+            dest_pin_points.stop,
+            source_pin_points.start,
+            dest_pin_points.stop,
+        }
+
+        source_port_id = source_pin_id_seq.port_id
+        dest_port_id = dest_pin_id_seq.port_id
+
+        port_pair_region[(source_port_id, dest_port_id)] = RectangularPrism(
+            Pos.elem_min(*relevant_points),
+            Pos.elem_max(*relevant_points),
+        )
+
+    return sum(
+        region.intersects(
+            other_regions := CompositeRegion(
+                tuple(
+                    region
+                    for other_port_pair, other_region in port_pair_region.items()
+                    if other_port_pair != port_pair
+                )
             )
-        except BussingError:
-            record_dest_pin_bus_failure(pin_pos_pair.dest_pin_id)
-            path = None
-            # raise
-            # No more paths will be attempted.
-            # break
-
-        bus_path[pin_pos_pair.dest_pin_id] = path
-        if path is not None:
-            wire_regions = wire_regions | wire_region(path)
-
-    return bus_path
-
-
-def mutated_relaxed_bus_paths(
-    instance_regions: Region,
-    pin_pos_pairs: set[PinPosPair],
-    bus_paths: PartialBusPaths,
-    collision_cost: float,
-) -> PartialBusPaths:
-    pin_pos_pair_to_update = choice(sorted(pin_pos_pairs))
-
-    wire_regions = reduce(
-        operator.or_,
-        (
-            wire_region(path)
-            for dest_pin_id, path in bus_paths.items()
-            if dest_pin_id != pin_pos_pair_to_update.dest_pin_id
-            if path is not None
-        ),
-    )
-
-    try:
-        new_path = relaxed_bus_path(
-            instance_regions=instance_regions,
-            wire_regions=wire_regions,
-            source_point=pin_pos_pair_to_update.source_pin_pos,
-            dest_point=pin_pos_pair_to_update.dest_pin_pos,
-            collision_cost=collision_cost,
         )
-    except BussingError:
-        # raise
-        new_path = None
-
-    return {**bus_paths, pin_pos_pair_to_update.dest_pin_id: new_path}
-
-
-@dataclass
-class HerdPathFindingProblem(LocalSearchProblem[PartialBusPaths]):
-    netlist: Netlist
-    placement: InstancePlacement
-    collision_cost: float = 3
-
-    @property  # type: ignore
-    @first_id_cached
-    def instance_regions(self) -> Region:
-        return placement_region(self.netlist, self.placement).xz_padded(1)
-
-    @property  # type: ignore
-    @first_id_cached
-    def pin_pos_pairs(self) -> set[PinPosPair]:
-        return set(source_dest_pin_pos_pairs(self.netlist, self.placement))
-
-    def random_solution(self) -> PartialBusPaths:
-        return random_relaxed_bus_paths(
-            self.instance_regions,
-            self.pin_pos_pairs,
-            collision_cost=self.collision_cost,
-        )
-
-    def mutated_solution(self, solution: PartialBusPaths) -> PartialBusPaths:
-        return mutated_relaxed_bus_paths(
-            self.instance_regions,
-            self.pin_pos_pairs,
-            solution,
-            collision_cost=self.collision_cost,
-        )
-
-    def solution_cost(self, solution: PartialBusPaths) -> float:
-        return collision_count(solution) + 1 + 8 * too_far_to_bus_count(solution)
-
-    def good_enough(self, solution: PartialBusPaths) -> bool:
-        return collision_count(solution) == 0
+        for port_pair, region in port_pair_region.items()
+    ) / len(port_pair_region)
 
 
 @first_id_cached
-def dest_pin_relaxed_bus_path(
-    netlist: Netlist,
-    placement: InstancePlacement,
-) -> PartialBusPaths:
-
-    problem = HerdPathFindingProblem(
-        netlist=netlist,
-        placement=placement,
-        collision_cost=3,
-    )
-
-    return sim_annealing_searched_solution(problem, total_rounds=150, restarts=1)
+def too_far_to_bus_count(pin_buses: PartialPinBuses) -> int:
+    return sum(bus is None for bus in pin_buses.values())
